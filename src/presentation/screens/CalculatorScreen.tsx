a6fb36360c259cf1b52578b36bb2a5604ee15533
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-05 16:43:04 -0500

    seccion 6 video 84. Botones y estilos

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable prettier/prettier */
 
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import {styles} from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
@@ -10,6 +10,12 @@ export const CalculatorScreen = () => {
       <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
         <Text style={styles.mainResult}> 15000</Text>
         <Text style={styles.subResult}> 15</Text>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>1</Text>
+        </Pressable>
       </View>
     </View>
   );
